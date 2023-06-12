d352326764ce587125078ca1f2394e449d7937a5
Melkijo <jonathanandaraa@gmail.com>
2023-06-12 13:09:21 +0800

    first commit

---
+++ src/content/data.js
@@ -0,0 +1,65 @@
+export const data = [
+    {
+        "name": "Photo 1",
+        "url": "../images/insta_(1).png",
+        "description": "Photo 1 description",
+        "date": "2020-01-01",
+        "type": "image/png",
+    },
+    {
+        "name": "Photo 2",
+        "url": "../images/insta_(2).png",
+        "description": "Photo 2 description",
+        "date": "2020-01-02",
+        "type": "image/png",
+    },
+    {
+        "name": "Photo 3",
+        "url": "../images/insta_(3).png",
+        "description": "Photo 3 description",
+        "date": "2020-01-03",
+        "type": "image/png",
+    },
+    {
+        "name": "Photo 4",
+        "url": "../images/insta_(4).png",
+        "description": "Photo 4 description",
+        "date": "2020-01-04",
+        "type": "image/png",
+    },
+    {
+        "name": "Photo 5",
+        "url": "../images/insta_(5).png",
+        "description": "Photo 5 description",
+        "date": "2020-01-05",
+        "type": "image/png",
+    },
+    {
+        "name": "Photo 6",
+        "url": "./images/insta_(6).png",
+        "description": "Photo 6 description",
+        "date": "2020-01-06",
+        "type": "image/png",
+    },
+    {
+        "name": "Photo 7",
+        "url": "../images/insta_(7).png",
+        "description": "Photo 7 description",
+        "date": "2020-01-07",
+        "type": "image/png",
+    },
+    {
+        "name": "Photo 8",
+        "url": "../images/insta_(8).png",
+        "description": "Photo 8 description",
+        "date": "2020-01-08",
+        "type": "image/png",
+    },
+    {
+        "name": "Photo 9",
+        "url": "../images/insta_(9).png",
+        "description": "Photo 9 description",
+        "date": "2020-01-09",
+        "type": "image/png",
+    }
+]
